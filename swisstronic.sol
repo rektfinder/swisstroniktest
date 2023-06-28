bbb048e9146fccadcafe3031327345a4215c2284
REKTD <102896551+rektfinder@users.noreply.github.com>
2023-06-28 15:40:45 +0700

    Add files via upload

---
+++ swisstronic.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Register {
+    string public github;
+    address public owner;
+    
+    struct Referral {
+        address referralAddress;
+        string referralString;
+    }
+    
+    Referral[] public referrals;
+    
+    constructor() {
+        github = "rektfinder" ;
+        owner = 0xfDF13576D654c42135B8fF3Aa1CB47b1BD6FB2c4;
+    }
+    
+    function addReferral(address _referralAddress, string memory _referralString) external {
+        require(msg.sender == owner, "Only the owner can add referrals.");
+        referrals.push(Referral(_referralAddress, _referralString));
+    }
+    
+    function totalReferrals() public view returns (uint256) {
+        return referrals.length;
+    }
+}
